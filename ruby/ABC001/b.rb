126df6551b7f1f860d598b6ca61c9f9295301a9a
kikunantoka <kikunantoka@gmail.com>
2014-04-23 23:53:41 +0900

    Fix ABC001_B in Ruby

--- ruby/ABC001/b.rb
+++ ruby/ABC001/b.rb
@@ -1,13 +1,14 @@
-m = gets.to_f
-m /= 1000;
-if m > 70
-  print("89\n")
-elsif m >= 35
-  printf("%02d\n",(m-30)/5+80.to_i)
-elsif m >= 6 && m <= 30
-  printf("%02d\n",m+50.to_i)
-elsif m >= 0.1 && m <= 5
-  printf("%02d\n",m*10.to_i)
+m = gets.to_i
+if m > 70000
+  puts "89"
+elsif m >= 35000
+  puts ((m/1000-30)/5+80)
+elsif m >= 6000 && m <= 30000
+  puts (m/1000+50)
+elsif m >= 1000
+  puts (m/100)
+elsif m >= 100
+  puts "0#{m/100}"
 else
-  print("00\n")
+  puts "00"
 end
