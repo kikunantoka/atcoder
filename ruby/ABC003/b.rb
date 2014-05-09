c46b83cd103e499e139397272ad38d2592e6cb05
kikunantoka <kikunantoka@gmail.com>
2014-05-09 17:19:59 +0900

    Fix ABC003_B in Ruby

--- ruby/ABC003/b.rb
+++ ruby/ABC003/b.rb
@@ -1,22 +1,13 @@
 s = gets.to_s
 t = gets.to_s
+atcoder = "atcoder"
 (0..s.length).each do |i|
   if s[i] != t[i]
-    if s[i] == '@'
-      if t[i] != 'a' && t[i] != 't' && t[i] != 'c' && t[i] != 'o' && t[i] != 'd' && t[i] != 'e' && t[i] != 'r'
-        puts("You will lose")
-        break
-      end
-    elsif t[i] == '@'
-      if s[i] != 'a' && s[i] != 't' && s[i] != 'c' && s[i] != 'o' && s[i] != 'd' && s[i] != 'e' && s[i] != 'r'
-        puts("You will lose")
-        break
-      end
-    else
-      puts("You will lose")
+    if !( s[i] == '@' && atcoder.include?(t[i]) ) && !( t[i] == '@' && atcoder.include?(s[i]) )
+      puts "You will lose"
       break
     end
   elsif i == s.length - 1
-    puts("You can win")
+    puts "You can win"
   end
 end
