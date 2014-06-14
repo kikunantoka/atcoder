8285df86224a6bfe916d35980057464395ee6bd0
kikunantoka <kikunantoka@gmail.com>
2014-06-14 21:35:33 +0900

    Participate in ARC025 in Ruby

--- ruby/ARC025/a.rb
+++ ruby/ARC025/a.rb
@@ -1,2 +1,12 @@
-x = gets.to_i
-y = gets.chomp.split(" ").map(&:to_i)
+def max(a, b)
+  (a > b) ? a : b
+end
+
+d = gets.chomp.split(" ").map(&:to_i)
+j = gets.chomp.split(" ").map(&:to_i)
+result = 0
+(d.length).times do |i|
+  result += max(d[i],j[i])
+end
+puts(result)
+
--- ruby/ARC025/b.rb
+++ ruby/ARC025/b.rb
@@ -1,2 +1,5 @@
-x = gets.to_i
-y = gets.chomp.split(" ").map(&:to_i)
+h,w = gets.chomp.split(" ").map(&:to_i)
+c = []
+h.times do |i|
+  c[i] = gets.chomp.split(" ").map(&:to_i)
+end
