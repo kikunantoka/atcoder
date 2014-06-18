3b14eacf61df3d2d24ed8b6d587072accec6e6aa
kikunantoka <kikunantoka@gmail.com>
2014-06-18 23:11:21 +0900

    Fix ABC025_B

--- ruby/ARC025/b.rb
+++ ruby/ARC025/b.rb
@@ -3,3 +3,24 @@ c = []
 h.times do |i|
   c[i] = gets.chomp.split(" ").map(&:to_i)
 end
+h.times do |i|
+  w.times do |j|
+    if(i%2==0)
+      if(j%2==1)
+        c[i][j] = -c[i][j]
+      end
+    else
+      if(j%2==0)
+        c[i][j] = -c[i][j]
+      end
+    end
+  end
+end
+
+h.downto(1) do |i|
+  w.downto(1) do |j|
+    if(i != j)
+      i.downto(1)
+    end
+  end
+end
