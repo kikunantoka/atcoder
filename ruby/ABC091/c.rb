38b01737e0d353fb83ee0c91666edb83bf267152
kikunantoka <kikunantoka@gmail.com>
2018-03-18 01:46:44 +0900

    Solve a C problem

--- ruby/ABC091/c.rb
+++ ruby/ABC091/c.rb
@@ -1,16 +1,75 @@
 n = gets.to_i
-blue_x = []
-blue_y = []
-red_x = []
-red_y = []
+reds = []
+blues = []
 counter = 0
 
-n.times.each do |i|
-  blue_x[i], blue_y[i] = gets.chomp.split(" ").map(&:to_i)
+n.times.each do
+  x, y = gets.chomp.split.map(&:to_i)
+  reds.push(x: x, y: y)
 end
 
-n.times.each do |i|
-  red_x[i], red_y[i] = gets.chomp.split(" ").map(&:to_i)
+n.times.each do
+  x, y = gets.chomp.split.map(&:to_i)
+  blues.push(x: x, y: y)
 end
 
-puts blue_x
+reds.sort! { |a, b| a[:x] <=> b[:x] }
+blues.sort! { |a, b| a[:x] <=> b[:x] }
+
+blues.each do |blue|
+  candidates = reds.select { |red| red[:x] < blue[:x] }.sort { |a, b| a[:y] <=> b[:y] }.reverse
+  candidates.each do |c|
+    next unless c[:y] < blue[:y]
+    counter += 1
+    reds.delete c
+    break
+  end
+end
+
+puts counter
+
+# 解説どおりに実装
+
+# n = gets.to_i
+# points = []
+# set = []
+# counter = 0
+
+# n.times.each do |i|
+#   x, y = gets.chomp.split(' ').map(&:to_i)
+#   points.push(x: x, y: y, color: 'red')
+# end
+
+# n.times.each do |i|
+#   x, y = gets.chomp.split(' ').map(&:to_i)
+#   points.push(x: x, y: y, color: 'blue')
+# end
+
+# points.sort! { |a, b| a[:x] <=> b[:x] }
+
+# points.each do |point|
+#   if point[:color] == 'red'
+#     set.push(point)
+#   else
+#     maximum = set.select { |red| red[:y] < point[:y] }.max { |a| a[:y] }
+#     unless maximum.nil?
+#       counter += 1
+#       set.delete_at(set.index(maximum))
+#     end
+#   end
+# end
+
+# puts counter
+
+# 下記のテストケースが通らない
+# 5
+# 0 0
+# 7 3
+# 2 2
+# 4 8
+# 1 6
+# 8 5
+# 6 9
+# 5 4
+# 9 1
+# 3 7
