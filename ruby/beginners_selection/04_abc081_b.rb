c1b86b32c07ff48c676ea85da30a80f8b072b6a6
kikunantoka <kikunantoka@gmail.com>
2018-05-19 16:21:46 +0900

    Refactor for using all? method

--- ruby/beginners_selection/04_abc081_b.rb
+++ ruby/beginners_selection/04_abc081_b.rb
@@ -1,8 +1,7 @@
 x = gets.to_i
 ys = gets.chomp.split.map(&:to_i)
 count = 0
-while(1) do
-  break unless ys.select { |y| y.odd? }.empty?
+while(ys.all?(&:even?)) do
   ys.map! { |y| y / 2 }
   count += 1
 end
