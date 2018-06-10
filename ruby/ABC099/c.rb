66455a9611ff8ef506e7b0a096cad44e15f5a06d
kikunantoka <kikunantoka@gmail.com>
2018-06-11 01:15:30 +0900

    Fix C problem

--- ruby/ABC099/c.rb
+++ ruby/ABC099/c.rb
@@ -1,26 +1,14 @@
 n = gets.to_i
-ds = [1, 6, 9, 6**2, 9**2, 6**3, 9**3, 6**4, 9**4, 6**5, 6**6, 9**5]
-count = 0
-while (n != 0) do
-  selected = ds.select do |d|
-    d <= n
+N = 100000
+memo = [0] * (N+1)
+(1..N+1).each do |i|
+  a = []
+  7.times do |j|
+    k = i - 6 ** j
+    a << memo[k] if k >= 0
+    k = i - 9 ** j
+    a << memo[k] if k >= 0
   end
-  if selected.count >= 3
-    j = selected[-2]
-    while j < selected[-1] do
-      j += selected[-2]
-    end
-    if j + selected[-2] != selected[-1] && n >= j && n < selected[-1] + selected[-2]
-      n -= selected[-2]
-      puts selected[-2]
-    else
-      n -= selected[-1]
-      puts selected[-1]
-    end
-  else
-    n -= selected.max
-    puts selected.max
-  end
-  count += 1
+  memo[i] = a.min + 1
 end
-puts count
+puts memo[n]
