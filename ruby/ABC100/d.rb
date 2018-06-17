1068901c2c42f33ee6c9a137e950cc85d96ad647
kikunantoka <kikunantoka@gmail.com>
2018-06-17 14:06:51 +0900

    Fix D problem

--- ruby/ABC100/d.rb
+++ ruby/ABC100/d.rb
@@ -1,35 +1,35 @@
 n, m = gets.chomp.split.map(&:to_i)
-x = []
-xt = []
-y = []
-yt = []
-z = []
-zt = []
-a = []
-result = 0
+if m == 0
+  puts 0
+  exit
+end
+a1 = []
+a2 = []
+a3 = []
+a4 = []
+a5 = []
+a6 = []
+a7 = []
+a8 = []
+r1 = r2 = r3 = r4 = r5 = r6 = r7 = r8 = 0
 n.times do |i|
-  x[i], y[i], z[i] = gets.chomp.split.map(&:to_i)
-  xt[i], yt[i], zt[i] = x[i], y[i], z[i]
-  a[i] = x[i].abs + y[i].abs + z[i].abs
+  x, y, z = gets.chomp.split.map(&:to_i)
+  a1[i] = x + y + z
+  a2[i] = x + y - z
+  a3[i] = x - y + z
+  a4[i] = x - y - z
+  a5[i] = -x + y + z
+  a6[i] = -x + y - z
+  a7[i] = -x - y + z
+  a8[i] = -x - y - z
 end
-(m - 1).times do |j|
-  index = a.index(a.max)
-  puts index
-  xx = x[index]
-  yy = y[index]
-  zz = z[index]
-  x.slice!(index)
-  y.slice!(index)
-  z.slice!(index)
-  a.slice!(index)
-  xt.slice!(index)
-  yt.slice!(index)
-  zt.slice!(index)
-  (n - (j + 1)).times do |i|
-    xt[i] += xx
-    yt[i] += yy
-    zt[i] += zz
-    a[i] = xt[i].abs + yt[i].abs + zt[i].abs
-  end
-end
-puts a.max
+puts [
+  a1.sort.reverse[0...m].inject(:+),
+  a2.sort.reverse[0...m].inject(:+),
+  a3.sort.reverse[0...m].inject(:+),
+  a4.sort.reverse[0...m].inject(:+),
+  a5.sort.reverse[0...m].inject(:+),
+  a6.sort.reverse[0...m].inject(:+),
+  a7.sort.reverse[0...m].inject(:+),
+  a8.sort.reverse[0...m].inject(:+)
+].max
