6ac82b70b291fdc021cd4a7196fd7063088d19bd
kikunantoka <kikunantoka@gmail.com>
2018-07-02 21:20:35 +0900

    Solve C problem

--- ruby/ABC102/c.rb
+++ ruby/ABC102/c.rb
@@ -3,17 +3,9 @@ as = gets.chomp.split.map(&:to_i)
 as.map!.with_index(1) do |a, i|
   a - i
 end
-total = as.select(&:positive?).inject(:+).to_i + as.select(&:negative?).inject(:+).to_i
-b = total.zero? ? 0 : total.abs / n
-res1 = as.map do |a|
-  (total.positive? ? a - b : a + b).abs
+as.sort!
+b = as.size % 2 == 0 ? as[as.size / 2 - 1, 2].inject(:+) / 2 : as[as.size / 2]
+as.map! do |a|
+  (a - b).abs
 end
-b += 1
-res2 = as.map do |a|
-  (total.positive? ? a - b : a + b).abs
-end
-b -= 2
-res3 = as.map do |a|
-  (total.positive? ? a - b : a + b).abs
-end
-puts [res1.inject(:+), res2.inject(:+), res3.inject(:+)].min
+puts as.inject(:+)
